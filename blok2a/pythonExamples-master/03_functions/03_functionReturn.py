f5908c3e46ad70a037a0adbc842d6d1563b445e8
Kaya Bax <kayawsm@hotmail.com>
2018-10-04 12:54:23 +0200

    Added pythonExamples-master, started One sample sequencer

---
+++ blok2a/pythonExamples-master/03_functions/03_functionReturn.py
@@ -0,0 +1,6 @@
+#declare a function that returns a textual sound
+def getTextualSound():
+  return "************* BANG *************"
+
+#call the function getTextualSound and write the returned string to the console
+print(getTextualSound())
