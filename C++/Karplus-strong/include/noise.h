16543d1f642cfc339aff99e0000ab4dfdcd304f7
Bas Maat <bjmaat@gmail.com>
2019-03-03 12:30:37 +0100

    tested more things fixed noise.h

--- C++/Karplus-strong/include/noise.h
+++ C++/Karplus-strong/include/noise.h
@@ -1,7 +1,6 @@
 #ifndef _NOISE_H_
 #define _NOISE_H_
 #include "oscillator.h"
-#include <vector>
 using namespace std;
 
 #define PI_2 6.28318530717959
@@ -13,8 +12,6 @@ public:
   Noise(double samplerate);
   ~Noise();
 
-  // ovverride calculate method
-  vector<double> sample_buffer;
   void calculate();
 };
 
